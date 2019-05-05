9e538e9320fd3a49c036a755e06d2d09540611f9
Ivey Topaz <iveytopaz@gmail.com>
2019-05-05 13:35:09 -0600

    Start page layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Helmet } from 'react-helmet';
+import Currencies from './components/Currencies';
+import NetWorth from './components/NetWorth';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Helmet>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+      </Helmet>
+      <div>Tracking your Networth</div>
+      <Currencies></Currencies>
+      <NetWorth></NetWorth>
     </div>
   );
 }
