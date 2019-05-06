a3f1979680100716bda47e4580e9adb8f13e6de9
Ivey Topaz <iveytopaz@gmail.com>
2019-05-05 23:09:21 -0600

    Implement recieving data and editing line values

--- src/reducers/netWorthReducer.js
+++ src/reducers/netWorthReducer.js
@@ -1,8 +1,45 @@
-export default (state = {}, action) => {
+import omit from 'lodash/omit';
+import { FETCH_NET_WORTH, EDIT_LINE } from '../actions/netWorthActions';
+
+export default (state = { loaded: false }, action) => {
   switch (action.type) {
-  case 'EDIT_LINE':
+  case EDIT_LINE: {
+    const { prop, lineId, value } = action.payload
+    const indexToEdit = state[prop].findIndex(({ id }) => id === lineId);
+    const array = state[prop]
     return {
-      result: action.payload
+      ...state,
+      [prop]: [
+        ...array.slice(0, indexToEdit),
+        {
+          ...array[indexToEdit],
+          value: Math.floor(parseFloat(value) * 100) / 100
+        },
+        ...array.slice(indexToEdit + 1)
+      ]
+    }
+  }
+  case FETCH_NET_WORTH:
+    if (action.status === 'fetching') {
+      return {
+        ...state,
+        loading: true
+      }
+    } else if (action.status === 'success') {
+      return { 
+        ...state,
+        ...omit(action.response, 'convertedHoldings'),
+        ...action.response.convertedHoldings,
+        loading: false,
+        loaded: true,
+        error: false 
+      }
+    } else {
+      return { 
+        ...state,
+        loading: false,
+        error: true 
+      }
     }
   default:
     return state
