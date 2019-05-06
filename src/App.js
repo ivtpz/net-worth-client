328174cef7c2c8f95b1e170946ed7d2b9feeebd2
Ivey Topaz <iveytopaz@gmail.com>
2019-05-05 22:59:57 -0600

    Calculate initial totals on app load

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import Currencies from './components/Currencies';
 import NetWorth from './components/NetWorth';
+import * as NetWorthActionCreators from './actions/netWorthActions';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Helmet>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-      </Helmet>
-      <div>Tracking your Networth</div>
-      <Currencies></Currencies>
-      <NetWorth></NetWorth>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.calculate();
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+        <Helmet>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+        </Helmet>
+        <div>Tracking your Networth</div>
+        <Currencies></Currencies>
+        <NetWorth></NetWorth>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => bindActionCreators(NetWorthActionCreators, dispatch)
+
+export default connect(() => ({}), mapDispatchToProps)(App);
