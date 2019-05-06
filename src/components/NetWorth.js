66291e89ce252976e053e7fc353b2b3935ec734b
Ivey Topaz <iveytopaz@gmail.com>
2019-05-05 23:14:02 -0600

    Remove console log

--- src/components/NetWorth.js
+++ src/components/NetWorth.js
@@ -9,46 +9,43 @@ const getCells = (resource, type) => [
   { ...resource, propertyName: type, isCurrency: true, isEditable: true } 
 ];
 
-const NetWorth = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <TableLine cells={[ { value: 'Net Worth' }, {}, {}, { value: props.netWorthTotal, isCurrency: true } ]} />
-      <TableLine cells={[ { value: 'Assets' }, {}, {}, {} ]} />
-      <TableLine cells={[ { value: 'Cash and Investments' }, {}, { value: 'Interest Rate' }, {} ]} />
-      {props.loaded ? props.assets.filter(({ type }) => type === 0).map(asset => (
-        <TableLine 
-          key={asset.name} 
-          cells={getCells(asset, 'assets')}
-        />
-      )) : <></>}
-      <TableLine cells={[ { value: 'Cash and Investments' }, {}, { }, {} ]} />
-      {props.loaded ? props.assets.filter(({ type }) => type === 1).map(asset => (
-        <TableLine 
-          key={asset.name} 
-          cells={getCells(asset, 'assets')}
-        />
-      )) : <></>}
-      <TableLine cells={[ { value: 'Total Assets' }, {}, {}, { value: props.assetsTotal, isCurrency: true } ]}/>
-      <TableLine cells={[ { value: 'Liabilities' }, {}, {}, {  } ]}/>
-      <TableLine cells={[ { value: 'Short Term Liabilities' }, { value: 'Monthly Payment' }, { value: 'Interest Rate' }, {} ]} />
-      {props.loaded ? props.liabilities.filter(({ type }) => type === 0).map(liability => (
-        <TableLine 
-          key={liability.name} 
-          cells={getCells(liability, 'liabilities')}
-        />
-      )) : <></>}
-      <TableLine cells={[ { value: 'Long Term Debt' }, {}, {}, {} ]} />
-      {props.loaded ? props.liabilities.filter(({ type }) => type === 1).map(liability => (
-        <TableLine 
-          key={liability.name} 
-          cells={getCells(liability, 'liabilities')}
-        />
-      )) : <></>}
-      <TableLine cells={[ { value: 'Total Liabilities' }, {}, {}, { value: props.liabilitiesTotal, isCurrency: true } ]}/>
-    </div>
-  )
-}
+const NetWorth = (props) => (
+  <div>
+    <TableLine cells={[ { value: 'Net Worth' }, {}, {}, { value: props.netWorthTotal, isCurrency: true } ]} />
+    <TableLine cells={[ { value: 'Assets' }, {}, {}, {} ]} />
+    <TableLine cells={[ { value: 'Cash and Investments' }, {}, { value: 'Interest Rate' }, {} ]} />
+    {props.loaded ? props.assets.filter(({ type }) => type === 0).map(asset => (
+      <TableLine 
+        key={asset.name} 
+        cells={getCells(asset, 'assets')}
+      />
+    )) : <></>}
+    <TableLine cells={[ { value: 'Cash and Investments' }, {}, { }, {} ]} />
+    {props.loaded ? props.assets.filter(({ type }) => type === 1).map(asset => (
+      <TableLine 
+        key={asset.name} 
+        cells={getCells(asset, 'assets')}
+      />
+    )) : <></>}
+    <TableLine cells={[ { value: 'Total Assets' }, {}, {}, { value: props.assetsTotal, isCurrency: true } ]}/>
+    <TableLine cells={[ { value: 'Liabilities' }, {}, {}, {  } ]}/>
+    <TableLine cells={[ { value: 'Short Term Liabilities' }, { value: 'Monthly Payment' }, { value: 'Interest Rate' }, {} ]} />
+    {props.loaded ? props.liabilities.filter(({ type }) => type === 0).map(liability => (
+      <TableLine 
+        key={liability.name} 
+        cells={getCells(liability, 'liabilities')}
+      />
+    )) : <></>}
+    <TableLine cells={[ { value: 'Long Term Debt' }, {}, {}, {} ]} />
+    {props.loaded ? props.liabilities.filter(({ type }) => type === 1).map(liability => (
+      <TableLine 
+        key={liability.name} 
+        cells={getCells(liability, 'liabilities')}
+      />
+    )) : <></>}
+    <TableLine cells={[ { value: 'Total Liabilities' }, {}, {}, { value: props.liabilitiesTotal, isCurrency: true } ]}/>
+  </div>
+);
 
 
 const mapStateToProps = (state, ownProps) => ({
